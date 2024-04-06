a51326a792ca82d7f03d4642d69bc18d8fff27cd
k3v <25158905+k3v53@users.noreply.github.com>
2024-04-06 18:41:10 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.steam_auto_thinker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:steam_auto_thinker/app/app.dart';
+import 'package:steam_auto_thinker/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
